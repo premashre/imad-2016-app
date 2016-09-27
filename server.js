6cf949cc7a14378d580bd7b40762c90fb030360c
premashre <premashree@gmail.com>
2016-09-27 17:57:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article-one', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
     });
     app.get('/article-two', function (req, res) {
-    res.send('Article two has been requested and will appear here');
+   res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
     });
     app.get('/article-three', function (req, res) {
-    res.send('Article three has been requested and will appear here');
+   res.sendFile(path.join(__dirname, 'ui', 'Article-three.html'));
     });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

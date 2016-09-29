dc3a1f08fec0311a16098a4ca80aa27e438537a9
premashre <premashree@gmail.com>
2016-09-29 11:40:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+var img = document.getElementbyID ("madi");
+var marginLeft = 0;
+function moveRight () {
+    marginLeft - marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onlcick = function () {
+    var interval = setInterval(moveRight, 50);
+};

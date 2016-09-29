1bd88892733b029e8d8372c766ca74a1cc802e01
premashre <premashree@gmail.com>
2016-09-29 09:27:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 app.get('/article-one', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
     });
-    app.get('/article-two', function (req, res) {
+app.get('/article-two', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
     });
     app.get('/article-three', function (req, res) {
